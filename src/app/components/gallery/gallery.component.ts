87e23a14f3cb5e01e64e8a982e6516d4cd6655e5
Gustavo Julián <gustavo.julian@clabsa.com.mx>
2021-11-05 13:32:20 -0600

    carrousel

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { DrupalNode } from '../../interfaces/node.interface';
 
 @Component({
   selector: 'app-gallery',
   template: `
-<section class="viveusa-feed">
+<section class="viveusa-feed" *ngIf="nodes.length > 0">
   <div class="main-title"><h4>{{title}}</h4></div>
-  <div class="content">
-    <div *ngFor="let node of nodes;" class="item">
+  <div class="arrow-left" (click)="scrollLeft($event)"><</div>
+  <div class="arrow-right" (click)="scrollRight($event)">></div>
+  <div class="content" #wrapperContent>
+    <div *ngFor="let node of nodes; let i = index;" class="item" #items>
       <div class="title">
         <h3><a href="{{node.url}}" target="_blank">{{node.title}}</a></h3>
       </div>
@@ -29,10 +31,23 @@ import { DrupalNode } from '../../interfaces/node.interface';
   </div>
 </section>
   `,
-  styles: []
+  styles: [`
+    .content{
+      display: flex;
+      flex-flow: row nowrap;
+      overflow-x: auto;
+    }
+    .item{
+      flex: 1 1 auto;
+    }
+  `]
 })
 export class GalleryComponent implements OnInit {
   @Input('title') title: string = '';
+  @ViewChild('wrapperContent') wrapperContent!:ElementRef;
+  @ViewChildren('items') items!:QueryList<ElementRef>;
+
+  currentItem: number = 0;
 
   _nodes: DrupalNode[] = [];
 
@@ -44,6 +59,41 @@ export class GalleryComponent implements OnInit {
 
   ngOnInit(): void {
     this.nodeService.loadNodes().subscribe(nodes => this._nodes = nodes);
+  }
+
+  scrollLeft(e:any) {
+    this.decrementCurrentItem();
+    const width = this.items.first.nativeElement.offsetWidth;
+    this.wrapperContent.nativeElement.scrollLeft = width * this.currentItem;
+  }
+
+  scrollRight(e: any) {
+    this.incrementCurrentItem();
+    const width = this.items.first.nativeElement.offsetWidth;
+
+    const viewPort = this.wrapperContent.nativeElement.offsetWidth;
+    const runScroll = width * this.currentItem;
+    const totalScrollScope = this.wrapperContent.nativeElement.scrollWidth // Stoping signal
+    const stop = (this._nodes.length * width) - viewPort;
+    console.table({runScroll, totalScrollScope, viewPort, stop});
+    if (runScroll < stop) {
+      this.wrapperContent.nativeElement.scrollLeft = width * this.currentItem; // Scroll to next element
+    } else {
+      this.wrapperContent.nativeElement.scrollLeft = width * this._nodes.length; // Scroll max offset default
+    }
+  }
+
+  incrementCurrentItem(){
+    this.currentItem++;
+    if (this.currentItem >= this._nodes.length-1) { // Last item doesn't count
+      this.currentItem = this._nodes.length-1;
+    }
+  }
+  decrementCurrentItem(){
+    this.currentItem--;
+    if (this.currentItem <= 0) {
+      this.currentItem = 0;
+    }
   }
 
 }
